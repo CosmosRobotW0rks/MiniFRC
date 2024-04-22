4b89640edd31c988295fc13019796300dcc8e948
Bora <89869983+BoRa-SY@users.noreply.github.com>
2024-04-22 22:54:39 +0300

    Initial Commit

---
+++ src/MiniFRC_FMS/MiniFRC_FMS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniFRC_FMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
